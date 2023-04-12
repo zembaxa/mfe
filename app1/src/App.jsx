fcb2af4aac109f61fab71f3838bfe44dc2a4f76c
zembaxa <g.zembala@gmail.com>
2023-04-12 12:02:08 +0200

    demo1

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import App2 from 'app2/App';
 
@@ -10,26 +8,12 @@ function App() {
   return (
     <>
       <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
         <h1>Application 1</h1>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
           </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
         </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </div>
       <App2 />
     </>
